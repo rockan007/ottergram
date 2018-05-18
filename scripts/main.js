c0945005669ce62e686095100f3b672f072aad5b
rockan007 <rockan007@gmail.com>
2018-05-18 11:53:52 +0800

    javascript 引入

---
+++ scripts/main.js
@@ -0,0 +1,74 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_IMAGE_TITLE = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = 27;
+
+function setDetails(imageUrl, titleText) {
+    'use strict'
+    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', imageUrl);
+
+    var detailTitle = document.querySelector(DETAIL_IMAGE_TITLE);
+    detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+    'use strict'
+    return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+    'use strict'
+    return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailFromThumb(thumbnail) {
+    'use strict'
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+    'use strict'
+    thumb.addEventListener('click', function (event) {
+        event.preventDefault();
+        setDetailFromThumb(thumb);
+        showDetails();
+    })
+}
+
+function getThumbNailsArray() {
+    'use strict'
+    var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    var thumbnailArray = [].slice.call(thumbnails);
+    return thumbnailArray;
+}
+
+function hiddenDetails() {
+    'use strict'
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+    'use strict'
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+
+function addKeyPressHandler() {
+    'use strict'
+    document.body.addEventListener('keyup', function (event) {
+        event.preventDefault();
+        console.log(event.keyCode);
+        if (event.keyCode === ESC_KEY) {
+            hiddenDetails();
+        }
+    })
+}
+
+function initializeEvents() {
+    'use strict'
+    var thumbnails = getThumbNailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+    addKeyPressHandler();
+}
+initializeEvents();
